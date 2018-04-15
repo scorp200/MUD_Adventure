7168ee47ad7b2c3df7743a0d4ccdd3f6773ac738
JackOatley <jack.f.oatley@gmail.com>
2018-04-15 22:18:22 +0100

    Changed World module to a IIFE
    Simply wrapped in an IIFE because in the browser, when the modules are just included on the page, they all share the same global scope. This prevents it.

--- shared/world.js
+++ shared/world.js
@@ -1,62 +1,66 @@
 
-/**
- * @constructor
- */
-var module = function( opts = {} ) {
+(function() {
 
-	// get values or set defaults
-	this.width = opts.width || 32,
-	this.height = opts.height || 32;
-	this.data = {};
-	
-	// fill map
-	for ( var x=0; x<this.width; x++ )
-	for ( var y=0; y<this.height; y++ ) {
-		this.data[x+"-"+y] = new module.Cell();
+	/**
+	 * @constructor
+	 */
+	var module = function( opts = {} ) {
+
+		// get values or set defaults
+		this.width = opts.width || 32,
+		this.height = opts.height || 32;
+		this.data = {};
+		
+		// fill map
+		for ( var x=0; x<this.width; x++ )
+		for ( var y=0; y<this.height; y++ ) {
+			this.data[x+"-"+y] = new module.Cell();
+		}
+
 	}
 
-}
-
-/**
- * Iterates through the map and builds a HTML string.
- */
-module.prototype.render = function() {
-	
-	//
-	var html = "<div>";
-	for ( var y=0; y<this.height; y++ ) {
-		html += "<div class='row'>";
-		for ( var x=0; x<this.width; x++ ) {
-			var cell = this.data[x+"-"+y];
-			html += "<div style=\"width: 12px; height: 12px; background-color: "+cell.draw[1]+"; background-image: url(test/codepage-437.png); background-position: -24px -132px;\"></div>";
+	/**
+	 * Iterates through the map and builds a HTML string.
+	 */
+	module.prototype.render = function() {
+		
+		//
+		var html = "<div>";
+		for ( var y=0; y<this.height; y++ ) {
+			html += "<div class='row'>";
+			for ( var x=0; x<this.width; x++ ) {
+				var cell = this.data[x+"-"+y];
+				html += "<div style=\"width: 12px; height: 12px; background-color: "+cell.draw[1]+"; background-image: url(test/codepage-437.png); background-position: -24px -132px;\"></div>";
+			}
+			html += "</div>";
 		}
 		html += "</div>";
+		
+		//
+		return html;
+
 	}
-	html += "</div>";
-	
-	//
-	return html;
 
-}
+	/**
+	 * @constructor
+	 */
+	module.Cell = function( opts = {} ) {
 
-/**
- * @constructor
- */
-module.Cell = function( opts = {} ) {
+		this.type = opts.type || "grass";
+		this.draw = module.Cell.mapping[this.type];
 
-	this.type = opts.type || "grass";
-	this.draw = module.Cell.mapping[this.type];
+	}
 
-}
+	// See https://www.martinstoeckli.ch/fontmap/fontmap.html reference
+	// Assume Courier New is used
+	module.Cell.mapping = {
+		"grass": [ "&#8718;", "#00ff00" ]
+	}
 
-// See https://www.martinstoeckli.ch/fontmap/fontmap.html reference
-// Assume Courier New is used
-module.Cell.mapping = {
-	"grass": [ "&#8718;", "#00ff00" ]
-}
-
-// export
-var __moduleName = "World";
-( typeof exports === "undefined" )
-	? window[__moduleName] = module
-	: exports = module;
+	// export
+	var __moduleName = "World";
+	( typeof exports === "undefined" )
+		? window[__moduleName] = module
+		: exports = module;
+		
+})();
