a774732472dc802bd0027d2f292f1e35a9afd4f5
JackOatley <jack.f.oatley@gmail.com>
2018-04-15 18:44:29 +0100

    Added command box + better "story" area

--- client/js/client.js
+++ client/js/client.js
@@ -3,12 +3,29 @@
 var domTools = document.getElementById( "tools" ),
 	domCharacter = document.getElementById( "character" ),
 	domMap = document.getElementById( "map" ),
-	domStory = document.getElementById( "story" ),
-	domCommand = document.getElementById( "command" );
-
-// placeholder stuff
-domStory.innerHTML = window.backToTheIpsum;
+	domStory = document.querySelector( "#story div" ),
+	domCommand = document.querySelector( "#command input" );
 
 //
 var world = new World();
 domMap.innerHTML = world.render();
+
+//
+domCommand.onkeydown = function( e ) {
+	if ( e.key === "Enter" ) {
+		
+		// scrolling element
+		var el = document.querySelector( "#story" )
+		var atBottom = ( el.scrollHeight - el.scrollTop === el.clientHeight );
+		
+		// grab command, "print" it and clear input
+		// Do something with command later
+		var cmd = domCommand.value;
+		domStory.innerHTML += "<p>" + cmd + "</p>"
+		domCommand.value = "";
+		
+		// if at bottom, stay there
+		if ( atBottom ) el.scrollTop = el.scrollHeight;
+		
+	}
+}
