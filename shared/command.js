736864a9337d1b37b1faf8060a386d967f7bf84a
JackOatley <jack.f.oatley@gmail.com>
2018-04-16 23:40:52 +0100

    Added new character commands + command input capture

--- shared/command.js
+++ shared/command.js
@@ -2,9 +2,15 @@
 (function() {
 
 	var command = {
+		_capture: null,
 
-		test: {
+		"test": {
 			_execute: exeTest
+		},
+		
+		"new": {
+			_condition: "start",
+			_execute: exeNew
 		}
 
 	}
@@ -14,16 +20,53 @@
 	 */
 	command.execute = function( cmd ) {
 		
-		if ( typeof command[cmd] !== "undefined" )
-			command[cmd]._execute();
-		else
-			Story.log( "Unknown command: " + cmd );
+		console.log( command._capture );
+		if ( command._capture === null ) {
+			
+			if ( typeof command[cmd] !== "undefined" )
+				command[cmd]._execute();
+			else
+				Story.log( "Unknown command: " + cmd );
+			
+		} else {
+		
+			if ( command._capture.check === null || command._capture.check( cmd ) ) {
+				command._capture.success();
+			} else {
+				command._capture.fail();
+			}
+		
+		}
 		
 	}
+	
+/** */
+function exeNew() {
+	Story.log( "Creating a new character..." );
+	Story.log( "Please enter the name of your character:" );
+	command._capture = {
+		check: function( x ) { return x !== "fuck"; },
+		success: exePassword,
+		fail: function() {
+			Story.log( "Terrible name! Try again:" );
+		}
+	}
+}
+	
+	/** */
+	function exePassword() {
+		Story.log( "Please enter a password:" );
+		command._capture = {
+			check: null,
+			success: function() {
+				Story.log( "You now exist!" );
+				command._capture = null;
+			},
+			fail: function() { Story.log( "Try again:" ); }
+		}
+	}
 
-	/**
-	 *
-	 */
+	/** */
 	function exeTest() {
 		Story.log( "Yep, the command system seems to be working..." );
 	}
