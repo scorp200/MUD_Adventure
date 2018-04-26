bbd3d57e89b7916a00cd9926183bddfbbd648b09
scorp200 <abca2004@gmail.com>
2018-04-26 02:07:23 -0400

    working player update
    fixed the issue with player render,
    and added a command moveChunk to move 64 cells in the specified direction

--- shared/world.js
+++ shared/world.js
@@ -5,8 +5,8 @@
         Simplex = require('../shared/simplex.js');
         Chunk = require('../shared/Chunk.js');
         Cell = require('../shared/Cell.js');
-		console.group = console.log;
-		console.groupEnd = console.log;
+        console.group = console.log;
+        console.groupEnd = console.log;
     }
 
     /**
@@ -19,7 +19,7 @@
         this.chunkHeight = opts.chunkHeight || 64;
         this.width = opts.width || 2;
         this.height = opts.height || 2;
-		this.dataMethod = 1;
+        this.dataMethod = 1;
         this.chunks = {};
         this.name = opts.name || 'world';
 
@@ -43,40 +43,42 @@
      */
     world.prototype.generate = function() {
 
-		console.group("generating world...");
-		var totalSize = this.width * this.height,
-			currentSize = 0;
-			
-		for (var x = 0; x < this.width; x++) {
-			for (var y = 0; y < this.height; y++) {
-				currentSize++;
-				var index = y * this.width + x;
-				console.log( "new chunk: " + index );
-				this.chunks[index] = new Chunk({
-					world: this,
-					x: x,
-					y: y,
-					width: this.chunkWidth,
-					height: this.chunkHeight,
-					dataMethod: this.dataMethod
-				});
-			}
-			console.log( "generation progress: " + ~~((currentSize / totalSize)*100) + "%" );
-		}
-		
-		console.groupEnd();
+            console.group("generating world...");
+            var totalSize = this.width * this.height,
+                currentSize = 0;
 
-	},
+            for (var x = 0; x < this.width; x++) {
+                for (var y = 0; y < this.height; y++) {
+                    currentSize++;
+                    var index = y * this.width + x;
+                    //console.log( "new chunk: " + index );
+                    this.chunks[index] = new Chunk({
+                        world: this,
+                        x: x,
+                        y: y,
+                        width: this.chunkWidth,
+                        height: this.chunkHeight,
+                        dataMethod: this.dataMethod
+                    });
+                }
+                var progress = ~~((currentSize / totalSize) * 100);
+                if (progress % 20 == 0)
+                    console.log("generation progress: " + progress + "%");
+            }
 
-	/**
-	 *
-	 */
-    world.prototype.getChunk = function(pos) {
-		var x = ~~(pos.x / this.chunkWidth),
-			y = ~~(pos.y / this.chunkHeight),
-			index = y * this.width + x;
-        return index;
-    }
+            console.groupEnd();
+
+        },
+
+        /**
+         *
+         */
+        world.prototype.getChunk = function(pos) {
+            var x = ~~(pos.x / this.chunkWidth),
+                y = ~~(pos.y / this.chunkHeight),
+                index = y * this.width + x;
+            return index;
+        }
 
     // export
     if (typeof module === "undefined")
