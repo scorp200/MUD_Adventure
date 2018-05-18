9f7857c50781c4f1ff179bc3fa44f866a6ab61dd
scorp200 <abca2004@gmail.com>
2018-05-18 12:26:39 -0400

    quick player fix...

--- server/modules/player.js
+++ server/modules/player.js
@@ -45,6 +45,10 @@ player.prototype.getStats = function() {
 	}
 }
 
+player.prototype.setPlayer = function(newPlayer) {
+	Object.assign(this, newPlayer);
+}
+
 player.prototype.resetPlayer = function(newPos = { x: 0, y: 0 }) {
 	this.hp = this.hunger = this.hydration = 100;
 	this.inventory = {};
